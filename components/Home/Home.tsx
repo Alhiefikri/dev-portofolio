c34034fab7e39560201b691ed6cfc536a6f7bba7
alhiefikri <alhiepedia@gmail.com>
2025-03-02 14:36:56 +0800

    feat: About Me Section

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,9 +1,11 @@
-import Hero from "./Hero.tsx/page";
+import About from "./About/About";
+import Hero from "./Hero.tsx/Hero";
 
 const Home = () => {
   return (
     <div className="overflow-hidden">
       <Hero />
+      <About />
     </div>
   );
 };
