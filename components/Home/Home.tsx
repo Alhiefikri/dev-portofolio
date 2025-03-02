7cfff86db6f2aca8330818406f3e19f5fb34d107
alhiefikri <alhiepedia@gmail.com>
2025-03-02 23:56:17 +0800

    feat: animated AOS section and finish Project

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useEffect } from "react";
 import About from "./About/About";
 import Blog from "./Blog/Blog";
 import Contact from "./Contact/Contact";
@@ -6,8 +9,23 @@ import Project from "./Project/Project";
 import Reviews from "./Reviews/Reviews";
 import Services from "./Services/Services";
 import Skills from "./Skills/Skills";
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
 
 const Home = () => {
+  useEffect(() => {
+    const initAOS = async () => {
+      await import("aos");
+      AOS.init({
+        duration: 1000,
+        easing: "ease",
+        once: true,
+        anchorPlacement: "top-bottom",
+      });
+    };
+    initAOS();
+  }, []);
+
   return (
     <div className="overflow-hidden">
       <Hero />
