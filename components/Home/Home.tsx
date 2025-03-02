549f389c94187c7c3f8c223ce57ff03f00793f22
alhiefikri <alhiepedia@gmail.com>
2025-03-02 14:55:58 +0800

    feat: Service Section

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,11 +1,13 @@
 import About from "./About/About";
 import Hero from "./Hero.tsx/Hero";
+import Services from "./Services/Services";
 
 const Home = () => {
   return (
     <div className="overflow-hidden">
       <Hero />
       <About />
+      <Services />
     </div>
   );
 };
